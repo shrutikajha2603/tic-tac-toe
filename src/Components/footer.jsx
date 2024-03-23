9271ace6ac12e0745c9446281cb0be01711a6518
Shrutika Jha <shrutikajha2603@gmail.com>
2024-03-23 18:37:59 +0530

    tic tac toe

---
+++ src/Components/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Made with ❤️ by Shrutika Jha</p>
+    </footer>
+  );
+}
+
+export default Footer;
